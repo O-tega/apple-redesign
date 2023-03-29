878ad50bf65fbafe96b5938ba04930814f601edf
O-tega <tega.myview@gmail.com>
2023-03-29 16:11:05 +0100

    new next app with typescript and tailwind

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import react from "react";
+
+const App = () => {
+	return (
+		<div className='text-center text-blue-700 font-bold my-10'>
+			This is a new next app
+		</div>
+	);
+};
+
+export default App;
